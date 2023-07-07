00d98b39762997e7460782a86e113a08c68eb8f4
Ana Nicolly <ananickbf@gmail.com>
2023-07-07 09:34:34 -0300

    codando o botão de controle de incremento e decremento

--- src/app/shared/botao-controle/botao-controle.component.ts
+++ src/app/shared/botao-controle/botao-controle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-botao-controle',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class BotaoControleComponent {
 
+  @Input() operacao: 'incrementar' | 'decrementar' = "incrementar";
+  @Input() src = '';
+  @Input() alt = '';
 }
